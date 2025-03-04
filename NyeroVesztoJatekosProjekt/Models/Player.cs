39835c8709849a5d826c4125802408f0d47ad52c
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2025-03-04 19:10:55 +0100

    2. feladat

--- NyeroVesztoJatekosProjekt/Models/Player.cs
+++ NyeroVesztoJatekosProjekt/Models/Player.cs
@@ -11,9 +11,10 @@ namespace NyeroVesztoJatekosProjekt.Models
         public string Name { get; set; }
         public string Email { get; }
         public int StartingScore { get; }
-        public int Score { get; set; }
-        public int WinCount { get; set; } = 0;
-        public int LossCount { get; set; } = 0;
+        public int Score { get; private set; }
+        public int WinCount { get; private set; } = 0;
+        public int LossCount { get; private set; } = 0;
+        public string IsWinningString => Name + " " + (WinCount > LossCount ? "nyerő" : "vesztő") + " játékos.";
 
         public Player(string name, string email, int startingScore)
         {
@@ -25,6 +26,19 @@ namespace NyeroVesztoJatekosProjekt.Models
             Email = email;
             StartingScore = startingScore;
             Score = startingScore;
+        }
+
+        public void ChangeScore(int point)
+        {
+            Score += point;
+            if(point > 0)
+            {
+                WinCount++;
+            }
+            else if(point < 0)
+            {
+                LossCount++;
+            }
         }
 
         public override string ToString()
--- NyeroVesztoJatekosProjekt/Program.cs
+++ NyeroVesztoJatekosProjekt/Program.cs
@@ -12,3 +12,12 @@ catch(Exception ex)
 
 Player janos = new Player("Játékos Jani", "jatekos.jani@nyertes.hu", 50);
 Console.WriteLine(janos);
+
+Console.WriteLine("\n");
+
+// 2. feladat
+janos.ChangeScore(20);
+janos.ChangeScore(30);
+Console.WriteLine($"{janos.Name} {janos.WinCount} alkalommal nyert!");
+janos.ChangeScore(-40);
+Console.WriteLine($"{janos.IsWinningString} Aktuális pontszáma: {janos.Score}\n");
