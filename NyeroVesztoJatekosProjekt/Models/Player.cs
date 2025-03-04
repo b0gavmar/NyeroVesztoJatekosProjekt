bab2f520ddb9e92c4ece7961d11e2bf34770ee1f
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2025-03-04 18:59:57 +0100

    1. feladat

---
+++ NyeroVesztoJatekosProjekt/Models/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NyeroVesztoJatekosProjekt.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Email { get; }
+        public int StartingScore { get; }
+        public int Score { get; set; }
+        public int WinCount { get; set; } = 0;
+        public int LossCount { get; set; } = 0;
+
+        public Player(string name, string email, int startingScore)
+        {
+            if(startingScore < 0)
+            {
+                throw new Exception("Nem kezdhet a játékos negatív pontszámmal!");
+            }
+            Name = name;
+            Email = email;
+            StartingScore = startingScore;
+            Score = startingScore;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Email}) -> {Score} pont";
+        }
+
+    }
+}
--- NyeroVesztoJatekosProjekt/Program.cs
+++ NyeroVesztoJatekosProjekt/Program.cs
@@ -1,2 +1,14 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using NyeroVesztoJatekosProjekt.Models;
+
+// 1. feladat
+try
+{
+    Player nandor = new Player("Negatív Nándor", "negativnandor@jatekos.hu", -1000);
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Player janos = new Player("Játékos Jani", "jatekos.jani@nyertes.hu", 50);
+Console.WriteLine(janos);
